6b88fbec119d947cccac1c19332163be970ed748
einhornimmond <einhornimmond@users.noreply.github.com>
2022-04-26 10:31:44 +0000

    deploy: 9f8645bb98bd1cbeee0a609bc040b336017034fe

--- search/all_0.js
+++ search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['addressalreadyexistexception_0',['AddressAlreadyExistException',['classmodel_1_1gradido_1_1_address_already_exist_exception.html',1,'model::gradido']]],
-  ['api_1',['API',['../md__a_p_i.html',1,'']]],
+  ['api_20for_20gradido_20blockchain_20connector_1',['API for Gradido Blockchain Connector',['../md__a_p_i.html',1,'']]],
   ['authenticatedencryption_2',['AuthenticatedEncryption',['class_authenticated_encryption.html',1,'']]]
 ];
--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['api_0',['API',['../md__a_p_i.html',1,'']]]
+  ['api_20for_20gradido_20blockchain_20connector_0',['API for Gradido Blockchain Connector',['../md__a_p_i.html',1,'']]]
 ];
